bb0e0fbbc17a03ab979f0b0a119f057d707e6df3
Vinicius Gabriel <viniciusrtycbn@gmail.com>
2024-04-02 07:38:47 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import requests
+import json
+
+url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
+reponse = requests.get(url)
+print(reponse)
+
+if reponse.status_code == 200:
+    dados_json = reponse.json()
+    dados_restaurante = {}
+    for item in dados_json:
+        nome_do_restaurante = item['Company']
+        if nome_do_restaurante not in dados_restaurante:
+            dados_restaurante[nome_do_restaurante] = []
+            
+        dados_restaurante[nome_do_restaurante].append({
+            "item": item['Item'],
+            "price": item['price'],
+            "description": item['description']
+        })
+else:
+    print(f'O erro foi {reponse.status_code}')
+
+for nome_do_restaurante, dados in dados_restaurante.items():
+    nome_do_arquivo = f'{nome_do_restaurante}.json'
+    with open(nome_do_arquivo,'w') as arquivo_restaurante:
+        json.dump(dados, arquivo_restaurante, indent=4)
